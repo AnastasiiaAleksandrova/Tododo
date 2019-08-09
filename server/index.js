2d08421f45c8e6856317cc78aafa85b63b7a3a62
AnastasiiaAleksandrova <47037922+AnastasiiaAleksandrova@users.noreply.github.com>
2019-08-09 17:36:40 +0300

    first model created

--- server/index.js
+++ server/index.js
@@ -10,5 +10,41 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true}).then(
     err => { console.log(err) }
   );
 
+const itemSchema = mongoose.Schema({
+    body: String
+});
+
+
+const Item = mongoose.model('Item', itemSchema);
+
+let firstItem = new Item({
+    body: 'Buy food'
+});
+
+firstItem.save().then(
+    entry => { console.log(entry) },
+    err => { console.log(err) }
+);
+
+
+
+// get all items
+app.get('/list', (req, res) => {
+
+})
+
+// create new item
+app.post('/new_item', (req, res) => {
+
+})
+
+// delete item
+app.delete('/remove_item', (req, res) => {
+
+})
+
+
+
+
 
 app.listen(PORT, () => console.log('Yes, Sir!'))
