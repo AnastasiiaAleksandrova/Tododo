e8c7cace1dcb4331413e3c2e6f616ac31930e191
AnastasiiaAleksandrova <47037922+AnastasiiaAleksandrova@users.noreply.github.com>
2019-09-11 15:51:26 +0300

    fix

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const cors = require('cors');
 const path = require('path');
 const longpoll = require('express-longpoll')(app, { DEBUG: true });
 
-app.use('/', express.static('client/build'));
+// app.use('/', express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const keys = require('./keys');
 
